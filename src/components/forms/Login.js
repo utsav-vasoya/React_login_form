b421a0333d6cab1ae51d2b833468f044da83a80a
Utsav Vasoya <utsavvasoya89@gmail.com>
2023-01-22 15:08:39 +0530

    add google login

--- src/components/forms/Login.js
+++ src/components/forms/Login.js
@@ -25,7 +25,7 @@ function Login() {
                 position: toast.POSITION.TOP_RIGHT
             })
         } else {
-            axios.post("https://react-form-backend.onrender.com/users/login", { email: login.email, password: login.password }).then((res) => {
+            axios.post("http://localhost:4000/users/login", { email: login.email, password: login.password }).then((res) => {
                 if (res.data.msg) {
                     toast.error(res.data.msg, {
                         position: toast.POSITION.TOP_RIGHT
@@ -46,6 +46,26 @@ function Login() {
             setLogin({ email: "", password: "" })
         }
     }
+    const googleClick = () => {
+        axios.get("http://localhost:4000/users/auth/google").then((res) => {
+            if (res.data.msg) {
+                toast.error(res.data.msg, {
+                    position: toast.POSITION.TOP_RIGHT
+                });
+            } else {
+                toast.success("Login Successfull.", {
+                    position: toast.POSITION.TOP_RIGHT
+                });
+                setTimeout(() => {
+                    window.location = "/home";
+                }, 1000)
+                localStorage.setItem('dataKey', JSON.stringify(res.data));
+                // navigate("/home")
+            }
+        }).catch((err) => {
+            console.log(err);
+        });
+    }
     return (
         <>
             <section className="vh-100">
@@ -53,9 +73,26 @@ function Login() {
                     <div className="row d-flex justify-content-center align-items-center h-100">
                         <div className="col-lg-12 col-xl-11">
                             <div className="card text-black" >
-                                <div className="card-body p-md-5">
+                                <div style={{ marginTop: '-20px' }} className="card-body p-md-5">
                                     <div className="row justify-content-center">
                                         <div className="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
+                                            <div style={{ marginLeft: '90px' }} className="d-flex flex-row align-items-center justify-content-center justify-content-lg-start">
+                                                <p className="lead fw-normal mb-0 me-3">Sign in with</p>
+                                                <button type="button" onClick={googleClick} className="btn btn-primary btn-floating mx-1">
+                                                    <i className="fa-brands fa-google"></i>
+                                                </button>
+
+                                                <button type="button" className="btn btn-primary btn-floating mx-1">
+                                                    <i className="fab fa-twitter"></i>
+                                                </button>
+
+                                                <button type="button" className="btn btn-primary btn-floating mx-1">
+                                                    <i className="fab fa-linkedin-in"></i>
+                                                </button>
+                                            </div>
+                                            <div className="divider d-flex align-items-center my-4">
+                                                <p className="text-center fw-bold mx-3 mb-0">Or</p>
+                                            </div>
                                             <p className="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Sign in</p>
                                             <form className="mx-1 mx-md-4">
 
@@ -90,7 +127,7 @@ function Login() {
 
                                         </div>
                                         <div className="col-md-9 col-lg-6 col-xl-5">
-                                            <img style={{ marginTop: "0px" }} src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
+                                            <img style={{ marginTop: "28.5px" }} src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
                                                 className="img-fluid" alt="Sample image" />
                                         </div>
                                     </div>
--- src/components/forms/Signup.js
+++ src/components/forms/Signup.js
@@ -26,7 +26,7 @@ const Signup = () => {
                 position: toast.POSITION.TOP_RIGHT
             })
         } else {
-            axios.post("https://react-form-backend.onrender.com/users/register", { name: signup.name, email: signup.email, password: signup.password, confirmPassword: signup.confirmPassword }).then((res) => {
+            axios.post("http://localhost:4000/users/register", { name: signup.name, email: signup.email, password: signup.password, confirmPassword: signup.confirmPassword }).then((res) => {
                 if (res.data.msg) {
                     toast.error(res.data.msg, {
                         position: toast.POSITION.TOP_RIGHT
@@ -53,7 +53,7 @@ const Signup = () => {
                     <div className="row d-flex justify-content-center align-items-center h-100">
                         <div className="col-lg-12 col-xl-11">
                             <div className="card text-black" >
-                                <div className="card-body p-md-5">
+                                <div style={{marginTop:'-50px'}} className="card-body p-md-5">
                                     <div className="row justify-content-center">
                                         <div className="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
                                             <p className="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Sign up</p>
